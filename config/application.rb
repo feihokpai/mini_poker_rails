c346b93351802ed3f13474e203da45cfe6f237a5
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-02-16 17:34:03 -0300

    Configuration for Rspec to be generated automatically.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module PokerRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories' 
+    
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
