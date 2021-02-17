ddcc8caad0dd09bed2806cf983046cc54aea29c9
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-02-17 14:20:09 -0300

    Some changes to reduce the code in Card class.

--- app/domain/card.rb
+++ app/domain/card.rb
@@ -10,11 +10,6 @@ class Card
         initialize_validations( stringCombination )
     end
 
-    # @Deprecated Use Card::VALID_NUMBERS instead
-    def self.allValidNumbers
-        return VALID_NUMERICAL_NUMBERS + VALID_LETTER_NUMBERS
-    end
-
     def ==( value)
         return false if not value.is_a?( Card )
         return false if value.number != @number
@@ -24,13 +19,18 @@ class Card
 
     private 
 
-    def errorMessageBase
+    def argumentErrorMessageBase        
         message = "Tried to create a card passing"
     end
 
-    def initialize_validations( stringCombination )        
-        raise "#{errorMessageBase} a non-String as parameter: #{stringCombination}" if not stringCombination.is_a?(String)
-        raise "#{errorMessageBase} a String with a number of characters different of 2: #{stringCombination}" if stringCombination.size != 2
+    def argumentError( message )
+        raise ArgumentError.new( "#{argumentErrorMessageBase} #{message}" )        
+    end
+
+    def initialize_validations( stringCombination ) 
+        argumentError( "a non-String as parameter: #{stringCombination}" ) if not stringCombination.is_a?(String)  
+        msgAboutNumberOfCharacters = "a String with a number of characters different of 2: #{stringCombination}"
+        argumentError( msgAboutNumberOfCharacters ) if stringCombination.size != 2
         firstChar = stringCombination[0]
         validateNumber( firstChar )
         secondChar = stringCombination[1]
@@ -39,15 +39,15 @@ class Card
 
     def validateNumber( value )
         cardNumber = value.upcase
-        validNumber = Card.allValidNumbers.include?( cardNumber )
-        raise "#{errorMessageBase} a invalid numerical value: #{value}" if not validNumber
+        validNumber = Card::VALID_NUMBERS.include?( cardNumber )
+        argumentError( "a invalid numerical value: #{value}" ) if not validNumber        
         @number = cardNumber
     end
 
     def validateSuit( value )
         cardSuit = value.upcase
         validSuit = Card::VALID_SUITS.include?( cardSuit )
-        raise "#{errorMessageBase} a invalid suit value: #{value}" if not validSuit
+        argumentError( "a invalid suit value: #{value}" ) if not validSuit        
         @suit = cardSuit
     end
 end
