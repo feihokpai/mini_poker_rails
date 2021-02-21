eb0402ebcefa0732b0642ad34bc8df9015a39daa
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-02-20 21:34:15 -0300

    Now the system is saving correctly the informations of the file in database.

--- app/daos/move_dao.rb
+++ app/daos/move_dao.rb
@@ -1,10 +1,39 @@
 class MoveDao
     def saveBestResult( handCards, deckCards, bestMove)
-        handCardsString = Card.convertCardsArrayToString( handCards )
-        deckCardsString = Card.convertCardsArrayToString( deckCards )
-        bestMoveString = Card.convertCardsArrayToString( bestMove )
-        params = { "hand_cards": handCardsString, "deck_cards": deckCardsString, "best_move_cards": bestMoveString}
-        newMove = MoveModel.new( params )
-        newMove.save()
+        ActiveRecord::Base.transaction do
+            initialHandModel = saveHand( handCards )
+            deckHandModel = saveHand( deckCards )
+            besHandModel = saveHand( bestMove )
+            moveModel = MoveModel.new( { initialHand: initialHandModel, deckHand: deckHandModel, bestHand: besHandModel } )
+            moveModel.save!()
+        end
+    end
+
+    private
+
+    def saveHand( cardsArray )
+        validate_saveHand( cardsArray )
+        newHandModel = HandModel.new
+        handCardModelsArray = []
+        for card in cardsArray
+            handCardModelsArray << createHandCardModel( card, newHandModel )
+        end
+        newHandModel.save!()
+        for handCardModel in handCardModelsArray
+            handCardModel.save!()
+        end
+        return newHandModel
+    end
+
+    def validate_saveHand( cardsArray )
+        ValidateUtil.raiseIfIsNotAnArrayWithOnly( cardsArray, Card )
+        ValidateUtil.raiseIfArrayHasADifferentSize( cardsArray, Hand::NUMBER_OF_CARDS )
+    end
+
+    def createHandCardModel( card, newHandModel )
+        suitModel = SuitModel.where( letter: card.suit ).first
+        cardModel = CardModel.where( { number: card.numberAsInteger, suit: suitModel } ).first
+        newCardHandModel = CardHandModel.new( card: cardModel, hand: newHandModel )
+        return newCardHandModel
     end
 end
--- app/models/move_model.rb
+++ app/models/move_model.rb
@@ -4,19 +4,7 @@ class MoveModel < ApplicationRecord
     belongs_to :deckHand, :class_name => "HandModel", :foreign_key => "deck_hand_id", required: true
     belongs_to :bestHand, :class_name => "HandModel", :foreign_key => "best_hand_id", required: true
 
-    def initialize( params={} )
-        validate_initialize( params )
-        super(params)
-    end
     
     private
 
-    def validate_initialize( params )
-        handCardsString = params[:hand_cards]
-        deckCardsString = params[:deck_cards]
-        bestMoveCardsString = params[:best_move_cards]
-        ValidateUtil.raiseIfValueIsNotA( handCardsString, String )
-        ValidateUtil.raiseIfValueIsNotA( deckCardsString, String )
-        ValidateUtil.raiseIfValueIsNotA( bestMoveCardsString, String )
-    end
 end
