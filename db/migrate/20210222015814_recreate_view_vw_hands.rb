2a71d7fe70effd9abb1bbaa5e571715dea56a4fe
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-02-21 23:06:55 -0300

    * I created migrations to recreate the views vw_moves and vw_hands.

---
+++ db/migrate/20210222015814_recreate_view_vw_hands.rb
@@ -0,0 +1,21 @@
+class RecreateViewVwHands < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      CREATE VIEW vw_hands
+      AS
+      select ch.hand_id
+          , GROUP_CONCAT( c.number|| SUBSTR(s.name, 1,1), "," ) as cards
+      from cards_hands ch
+          inner join cards c on c.id = ch.card_id    
+          inner join suits s on c.suit_id = s.id
+      group by ch.hand_id
+      ;    
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP VIEW IF EXISTS vw_hands;      
+    SQL
+  end
+end
