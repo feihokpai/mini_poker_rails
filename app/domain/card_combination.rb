e0e041b7dc671e6caa93b26bacf970d57413742b
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-02-21 21:26:40 -0300

    It was added the Move Domain class. Some methods from CardCombination was transferred to the new class.

--- app/domain/card_combination.rb
+++ app/domain/card_combination.rb
@@ -30,55 +30,7 @@ class CardCombination
         return self.moveWithNumericalCombinations( numericalCombinations )
     end
 
-
-    def self.bestMoveUsingInitialHandAndDeck( initialHand, deckHand )
-        self.validate_bestMoveUsingInitialHandAndDeck( initialHand, deckHand )
-        bestResults = []
-        bestResults[0] = self.bestMoveTradingNCards( handCards, deckCards, 0 )
-        bestResults[1] = self.bestMoveTradingNCards( handCards, deckCards, 1 )
-        bestResults[2] = self.bestMoveTradingNCards( handCards, deckCards, 2 )
-        bestResults[3] = self.bestMoveTradingNCards( handCards, deckCards, 3 )
-        bestResults[4] = self.bestMoveTradingNCards( handCards, deckCards, 4 )
-        bestResults[5] = self.bestMoveTradingNCards( handCards, deckCards, 5 )
-        orderedMoves = bestResults.sort_by { |result| result[:combination].pontuation }
-        return orderedMoves.last
-    end
-
     private
-
-    def self.validate_bestMoveUsingInitialHandAndDeck( initialHand, deckHand )
-        ValidateUtil.raiseIfValueIsNotA( initalHand, Hand )
-        ValidateUtil.raiseIfValueIsNotA( deckHand, Hand )
-    end
-
-    def self.bestMoveTradingNCards( initialHand, deckHand, numberOfCards )        
-        cardCombination = self.defineCombination( initialHand )  
-        bestResult = { :combination => cardCombination, :move => initialHand, :hand => initialHand, :deck => deckHand }
-        return bestResult if numberOfCards == 0
-
-        cardIndexes = [0,1,2,3,4]
-        possibleCombinationsArray = cardIndexes.combination( numberOfCards ).to_a()
-
-        for indexesToTrade in possibleCombinationsArray
-            newHand = self.tradeNCardsWithDeck( initialHand, deckHand, indexesToTrade )
-            newCombination = self.defineCombination( newHand )
-            if newCombination.pontuation > bestResult[:combination].pontuation
-                bestResult[:combination] = newCombination
-                bestResult[:move] = copyOfInitialHandCardsArray
-            end
-        end
-        return bestResult
-    end
-
-    def self.tradeNCardsWithDeck( initialHand, deckHand, indexesToTrade )
-        copyOfInitialHandCardsArray = initialHand.cards.clone()
-        nextDeckCardsIndex = 0
-        for index in indexesToTrade
-            copyOfInitialHandCardsArray[index] = deckHand.cards[ nextDeckCardsIndex ]                
-            nextDeckCardsIndex += 1
-        end
-        return Hand.new( copyOfInitialHandCardsArray )
-    end
 
     def self.moveWithNumericalCombinations( numericalCombinations )   
         return ONE_PAIR if numericalCombinations.size == 2
